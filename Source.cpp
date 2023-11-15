9b44a6f4048879ef7705dc64e396ebcc50466897
Juan Angulo <juancarlosanguloabud@gmail.com>
2023-11-15 16:56:47 -0500

    Agregar archivos de proyecto.

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+//import header file
+#include "libreria.h"
+
+
+//void imprimirPersonaje() {
+//	cout << " O" << endl;
+//	cout<< "/|\\" << endl;
+//	cout << "/ \\" << endl;
+//
+//}
+
+int main() {
+    Console::SetWindowSize()
+    system("pause");
+}
---
+++ libreria.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+#define ALTO_VENTANA 80
+#define LARGO_VENTANA 40
+
+using namespace std;
+using namespace System;
+
